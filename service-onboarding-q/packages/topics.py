18edbc76b9f16ba3a3128659944dcbb9384927e9
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-08-09 01:22:32 +0530

    Added Service Onboarding Init Code

---
+++ service-onboarding-q/packages/topics.py
@@ -0,0 +1,29 @@
+# from kafka.admin import KafkaAdminClient, NewTopic
+from confluent_kafka.admin import AdminClient, NewTopic
+
+
+class Topics:
+
+    def __init__(self, kafkaClient: AdminClient) -> None:
+        self.kafkaClient: AdminClient = kafkaClient
+
+    def create(self, topic: str, partitions: int = 1, replication_factor: int = 1) -> None:
+        newTopic = [NewTopic(topic, partitions, replication_factor)]
+        topics = self.kafkaClient.create_topics(
+            new_topics=newTopic,
+            validate_only=False
+        )
+
+        for topic, f in topics.items():
+            try:
+                f.result()  # The result itself is None
+                print("Topic {} created".format(topic))
+            except Exception as e:
+                print("Failed to create topic {}: {}".format(topic, e))
+
+
+admin_client = AdminClient(
+    {"bootstrap.servers": "localhost:9092"})
+
+topics = Topics(admin_client)
+topics.create(topic='test-topic')
