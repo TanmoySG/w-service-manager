0c49aa638f5a819eea66e7dc47118cc3013f32da
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-08-10 00:36:21 +0530

    Added Start-Up Package for on-start actions

---
+++ service-onboarding-q/packages/startup.py
@@ -0,0 +1,13 @@
+from packages.topics import Topics
+
+class STARTUP:
+    
+    def __init__(self, configuration) -> None:
+        self.configuration = configuration
+        
+    def execute(self) -> None:
+        return 0
+    
+    def createTopicOnStartUp(self, topics) -> None:
+        
+        return 0
