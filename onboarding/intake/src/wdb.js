e17ec3f8b01f9be1d29ad2aacd9f2acc1c8bcd40
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-08-16 01:12:59 +0530

    Added Updated, Delete and View WDB Data Helpers #3

--- onboarding/intake/src/wdb.js
+++ onboarding/intake/src/wdb.js
@@ -7,6 +7,26 @@ export default class WDB {
         this.wdbEndpoint = `https://wdb.tanmoysg.com/connect?cluster=${configurations.cluster}&token=${configurations.token}`;
     }
 
+    getData(collection, callback) {
+        fetch(this.wdbEndpoint, {
+            method: "POST",
+            body: JSON.stringify({
+                "action": "get-data",
+                "payload": {
+                    "database": this.database,
+                    "collection": collection
+                }
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json()
+        }).then(function (response) {
+            callback(response.data, response.schema)
+        }).catch(function (error) {
+            callback(error)
+        })
+    }
+
     addData(collection, data, callback) {
         fetch(this.wdbEndpoint, {
             method: "POST",
@@ -28,16 +48,52 @@ export default class WDB {
         })
     }
 
-    deleteData(collection, data, callback) {
-
+    deleteData(collection, maker, callback) {
+        fetch(this.wdbEndpoint, {
+            method: "POST",
+            body: JSON.stringify({
+                "action": "delete-data",
+                "payload": {
+                    "database": this.database,
+                    "collection": collection,
+                    "marker": `${maker.Key} : ${maker.Value}`
+                }
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json()
+        }).then(function (response) {
+            callback(response)
+        }).catch(function (error) {
+            callback(error)
+        })
     }
+
+    updateData(collection, maker, data,callback) {
+        fetch(this.wdbEndpoint, {
+            method: "POST",
+            body: JSON.stringify({
+                "action": "update-data",
+                "payload": {
+                    "database": this.database,
+                    "collection": collection,
+                    "marker": `${maker.Key} : ${maker.Value}`,
+                    "data": data
+                }
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json()
+        }).then(function (response) {
+            callback(response)
+        }).catch(function (error) {
+            callback(error)
+        })
+    }
+
 }
-// const wdb = new WDB({
-//     "cluster": "",
-//     "token": "",
-//     "database": "",
-//     "colllection": ""
-// })
+
+// const wdb = new WDB({})
 
 // const sampledata = {
 //     "requestID": "1234",
@@ -49,5 +105,27 @@ export default class WDB {
 // }
 
 // wdb.addData("IntakeRequest-Stage", sampledata, function (resp) {
+//     console.log(resp)
+// })
+
+// wdb.deleteData("IntakeRequest-Stage", {
+//     "Key": "requestID",
+//     "Value" : "1234"
+// }, function (resp) {
+//     console.log(resp)
+// })
+
+// wdb.getData("IntakeRequest-Stage", function (data, schema) {
+//     console.log(data)
+// })
+
+// const sampledatachange = {
+//     "serviceOwners": ["trf", "rdf"]
+// }
+
+// wdb.updateData("IntakeRequest-Stage", {
+//     "Key": "requestID",
+//     "Value" : "1234"
+// }, sampledatachange,function (resp) {
 //     console.log(resp)
 // })
