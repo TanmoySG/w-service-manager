4ade38d269aadf73601f6e99de0bcb929ad0a3ff
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-08-18 10:36:42 +0530

    Updated Contract onfiguration Format #3

--- onboarding/intake/src/contract.js
+++ onboarding/intake/src/contract.js
@@ -1,17 +1,29 @@
 import { v4 as uuidv4 } from 'uuid';
 import JSONSchema from './helpers/schema.js';
 import isEmail from '@nickgatzos/is-email'
+import Kafka from './helpers/kafka.js';
+import WDB from './helpers/wdb.js';
 
 class Contract {
-    constructor(schema, dbconfig) {
-        this.schema = schema
-        this.jschema = new JSONSchema(this.schema)
+    constructor(configuration) {
+        this.config = configuration
 
-        this.dbconfig = dbconfig
+        this.schema = this.config.schema
+        this.jsonSchemaClient = new JSONSchema(this.schema)
+
+        this.kafkaConfig = this.config.kafkaConfig
+        this.kafkaClient = new Kafka(this.kafkaConfig)
+
+        this.dbconfig = this.config.dbconfig
+        this.dbClient = new WDB(this.dbconfig)
+    }
+
+
+    id() {
+        return `contract-${uuidv4()}`
     }
 
     new(contract, uuid) {
-        // console.log(this.validate(contract))
         if (this.validate(contract) == false) {
             return false
         }
@@ -20,37 +32,51 @@ class Contract {
         return contract
     }
 
+    register(contract) {
+        const uuid = contract.contractID
+        this.kafkaClient.produce(this.config.kafkaConfig.topic, uuid,JSON.stringify(contract))
+        const updatedRequestData = {
+            timestamp: Date.now(),
+            status: 'proccessed',
+        }
+        const marker = {
+            "Key": "requestID",
+            "Value": contract.request_id
+        }
+        this.dbClient.updateData(this.dbconfig.collection, marker, updatedRequestData, function (resp) {
+            console.log(resp)
+        })
+    }
+
     validate(data) {
-        if (this.jschema.validate(data)) {
-            data.developer.admin.forEach(mailid => {
-                console.log(mailid, isEmail(mailid))
+        if (this.jsonSchemaClient.validate(data)) {
+            for (var i = 0; i < data.developer.admin.length; i++) {
+                const mailid = data.developer.admin[i]
                 if (isEmail(mailid) == false) {
                     return false;
                 }
-            });
-            data.developer.contributor.forEach(mailid => {
-                console.log(isEmail(mailid))
+            }
+            for (var i = 0; i < data.developer.admin.length; i++) {
+                const mailid = data.developer.admin[i]
                 if (isEmail(mailid) == false) {
                     return false;
                 }
-            });
+            }
             return true;
         } else {
             return false
         }
     }
 
-    id() {
-        return `contract-${uuidv4()}`
-    }
-
 }
 
-// const cntr = new Contract('/Users/tanmoysg/Work/Projects/wunder/w-service-manager/schema/service-onboarding/contract.intake.schema.json')
+
+// const config = {}
+// const cntr = new Contract(config)
 
 // const smk = {
 //     "kind": "contract.intake.service-onboarding",
-//     "request_id": "123",
+//     "request_id": "d9f28bbf-f5f4-470a-ab0b-bc7cffbf3fbb",
 //     "service": {
 //         "name": "placeholders",
 //         "respository": "link.to.placeholders",
@@ -70,9 +96,10 @@ class Contract {
 //         }
 //     },
 //     "developer": {
-//         "admin": ["tanmoysggmailcom", "tsgupta@mail.com"],
+//         "admin": ["tanmoysg@gmail.com", "tsgupta@mail.com"],
 //         "contributor": []
 //     }
 // }
 
-// console.log(cntr.new(smk, cntr.id()))
+// const cntrct = cntr.new(smk, cntr.id())
+// cntr.register(cntrct)
