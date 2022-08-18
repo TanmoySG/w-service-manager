845f035f173ac54d91558c7c361da976dfc8bd57
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-08-18 10:37:36 +0530

    Updated Configuration Format and UUID Format #3

--- onboarding/intake/src/intake.js
+++ onboarding/intake/src/intake.js
@@ -3,17 +3,18 @@ import JSONSchema from './helpers/schema.js';
 import WDB from './helpers/wdb.js';
 
 export default class Request {
-    constructor(schema, dbconfig) {
-        this.schema = schema
-        this.jschema = new JSONSchema(this.schema)
+    constructor(configuration) {
+        this.config = configuration
 
-        this.dbconfig = dbconfig
-        this.db = new WDB(this.dbconfig)
+        this.schema = this.config.schema
+        this.jsonSchemaClient = new JSONSchema(this.schema)
 
+        this.dbconfig = this.config.dbconfig
+        this.dbClient = new WDB(this.dbconfig)
     }
 
     new(data, uuid) {
-        if (this.jschema.validate(data)) {
+        if (this.jsonSchemaClient.validate(data)) {
             const request = {
                 requestID: uuid,
                 timestamp: Date.now(),
@@ -22,16 +23,16 @@ export default class Request {
                 serviceOwners: data.service_owners,
                 requestType: data.request_type
             }
-            this.db.addData(this.dbconfig.collection, request, function (resp) {
+            this.dbClient.addData(this.dbconfig.collection, request, function (resp) {
                 console.log(resp)
             })
         } else {
-            console.log(false) 
+            console.log(false)
         }
     }
 
     id() {
-        return uuidv4();
+        return `request-${uuidv4()}`
     }
 }
 
