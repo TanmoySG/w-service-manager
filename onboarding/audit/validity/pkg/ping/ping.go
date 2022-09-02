9ba582788ba83d111956136746169f41d8801c70
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-09-03 00:39:46 +0530

    Replaced response.Status to response.StatusCode

--- onboarding/audit/validity/pkg/ping/ping.go
+++ onboarding/audit/validity/pkg/ping/ping.go
@@ -15,24 +15,24 @@ const (
 	POST = "post"
 )
 
-func getCall(uri string, payload *map[string]interface{}) string {
+func getCall(uri string, payload *map[string]interface{}) int {
 	response, _ := http.Get(uri)
-	return response.Status
+	return response.StatusCode
 }
 
-func postCall(uri string, payload *map[string]interface{}) string {
+func postCall(uri string, payload *map[string]interface{}) int {
 	requestJSON, _ := json.Marshal(payload)
 	response, _ := http.Post(uri, ContentType, bytes.NewBuffer(requestJSON))
-	return response.Status
+	return response.StatusCode
 }
 
-func Ping(uri string, method string, payload *map[string]interface{}) (string, error) {
+func Ping(uri string, method string, payload *map[string]interface{}) (int, error) {
 	switch strings.ToLower(method) {
 	case GET:
 		return getCall(uri, payload), nil
 	case POST:
 		return postCall(uri, payload), nil
 	default:
-		return "", fmt.Errorf("error pinging")
+		return 0, fmt.Errorf("error pinging")
 	}
 }
