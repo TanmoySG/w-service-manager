c9fa539a32a6a13b2de800c934994903cedecaf1
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-09-03 02:00:31 +0530

    Updated NameValidation function #11

--- onboarding/audit/validity/internal/checks/name.go
+++ onboarding/audit/validity/internal/checks/name.go
@@ -1,17 +1,33 @@
 package checks
 
 import (
-	"validity/internal/service-directory"
+	serviceDirectory "validity/internal/service-directory"
+	"validity/spec/validity"
 
 	"golang.org/x/exp/slices"
 )
 
-func (c Client) CheckNameExistenceInServicesDirectory(name string) bool {
+var NameInvalidityReason = "Name Already Exists."
+
+func (c Client) CheckNameExistenceInServicesDirectory(name string) validity.Validations {
+	var overallValidity bool = Valid
+	var overallValidityReason string = "Valid"
+
 	sd := serviceDirectory.MockClient{
 		WDBClient:  c.WDBClient,
 		Collection: "servicesDirectory",
 	}
-
 	servicesList := sd.GetServiceNameList()
-	return !slices.Contains(*servicesList, name)
+
+	if !slices.Contains(*servicesList, name) {
+		overallValidity = Invalid
+		overallValidityReason = NameInvalidityReason
+	}
+
+	nameValidity := validity.Validations{
+		Error: overallValidityReason,
+		Valid: overallValidity,
+	}
+
+	return nameValidity
 }
