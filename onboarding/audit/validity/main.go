c5326b976152c609caf11119d7ca02b455d1bfbf
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-09-03 13:31:19 +0530

    Added validity call to main.go

--- onboarding/audit/validity/main.go
+++ onboarding/audit/validity/main.go
@@ -1,122 +1,24 @@
 package main
 
 import (
-	// "fmt"
-	// "time"
-
-	// "encoding/json"
-	// "validity/pkg/kafka"
 	"fmt"
-	// "validity/pkg/config"
-	// "validity/pkg/service-directory"
-	// "validity/pkg/wdb"
+	"validity/pkg/config"
 
-	"validity/internal/control-list"
+	AuditValidity "validity/internal/audit-validity"
 )
 
 func main() {
 
-	fmt.Print("Placeholder")
+	fmt.Print("Audit Validation Started")
 
-	// broker := []string{
-	// 	0: "localhost:9092",
-	// }
-	// clientID := "hsgfsd"
-	// deadline := time.Now().Add(10 * time.Second)
+	c, _ := config.LoadConfigFromFile("./config/secrets/config.secrets.json")
 
-	// kc := kafka.Client{
-	// 	Brokers:  broker,
-	// 	ClientID: clientID,
-	// 	ReadDeadline: deadline,
-	// }
-
-	// tpc := "intake"
-	// msg := make(map[string]interface{})
-
-	// msg["test"] = "pass"
-	// msgData, err := json.Marshal(msg)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
-	// kc.Producer(tpc, []byte("1c"), []byte(msgData))
-
-	// kc.Consumer(tpc, func(key, value []byte) {
-	// 	fmt.Printf("%s : %s", key, value)
-	// })
-	// fmt.Print("test")
-
-	// c, _ := config.LoadConfigFromFile("./config/secrets/config.secrets.json")
-
-	// db := "mockServiceDirectory" // c.Dbconfig.Database
-
-	// w := wdb.Client{
-	// 	Cluster:  c.Dbconfig.Cluster,
-	// 	Token:    c.Dbconfig.Token,
-	// 	Database: &db,
-	// }
-
-	// collection := "IntakeRequest-Stage"
-
-	// marker := make(map[string]string)
-	// marker["Key"] = "requestID"
-	// marker["Value"] = "request-2b3f501d-376a-4cf3-8e85-b3c1fe0cc4df"
-
-	// w.GetData(collection, marker, func(resp wdb.ResponseBody, err error) {
-	// 	fmt.Println(resp, err)
-	// })
-
-	// data := wdb.Data{
-	// 	"test" : "1",
-	// 	"result" : "pass",
-	// }
-
-	// collection = "testSpace"
-
-	// w.AddData(collection, data, func(rd wdb.ResponseBody, err error) {
-	// 	fmt.Println(rd, err)
-	// } )
-
-	// marker["Key"] = "_id"
-	// marker["Value"] = "J8fAtnxVdNPkoxhAne2tzB"
-
-	// data = wdb.Data{
-	// 	"result" : "failedddd",
-	// }
-
-	// w.UpdateData(collection, marker, data, func(rb wdb.ResponseBody, err error) {
-	// 	fmt.Println(rb, err)
-	// } )
-
-	// w.DeleteData(collection, marker, func(rb wdb.ResponseBody, err error) {
-	// 	fmt.Println(rb, err)
-	// } )
-
-	// s := schema.SchemaValidator{
-	// 	Schema:   schema.LoadFromFile("../../../schema/service-onboarding/validity.audit.schema.json"),
-	// 	Document: schema.LoadFromFile("../../../schema/service-onboarding/contract.intake.schema.json"),
-	// }
-	// flag, _ := s.Validate()
-
-	// fmt.Println(flag)
-
-	// sd := serviceDirectory.MockClient{
-	// 	WDBClient:  w,
-	// 	Collection: "servicesDirectory",
-	// }
-
-	// services := sd.GetServiceNameList()
-	// for _, service := range *services {
-	// 	fmt.Println(service)
-	// }
-
-	cl, _ := controlList.LoadControlList("/Users/tanmoysg/Work/Projects/wunder/w-service-manager/onboarding/audit/validity/resources/templates/control.list.json")
-	ac, err := controlList.LoadControlList("./resources/examples/assigned.control.json")
-	if err != nil {
-		fmt.Printf("%s", err)
+	av := AuditValidity.AuditValidityClient{
+		Config:           *c,
+		ServiceDirectory: "",
+		ControlList:      "resources/templates/control.list.json",
+		SourceTopic:      "intake",
+		SinkTopic:        "audit",
 	}
-
-	if cl.ValidateAccessForAllFields(ac) {
-		fmt.Println("Valid")
-	}
+	av.RunAuditValidity()
 }
