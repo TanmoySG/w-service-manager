dda25b760ae6dec4580d4c678fc207d8ef216431
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-09-03 10:53:50 +0530

    Added GetContractValidity func

--- onboarding/audit/validity/internal/checks/checks.go
+++ onboarding/audit/validity/internal/checks/checks.go
@@ -2,10 +2,12 @@ package checks
 
 import (
 	"validity/pkg/wdb"
+	"validity/spec/contract"
+	"validity/spec/validity"
 )
 
 type Client struct {
-	WDBClient wdb.Client
+	WDBClient   wdb.Client
 	ControlList string
 }
 
@@ -13,3 +15,27 @@ var (
 	Valid   = true
 	Invalid = false
 )
+
+func (c Client) GetContractValidity(contract contract.Contract) validity.Checks {
+
+	overallContractValidity := Valid
+
+	nameValidation := c.CheckNameExistenceInServicesDirectory(contract.Service.Name)
+	repositoryValidation := c.CheckRepositoryStatus(*contract.Service.Repository)
+	accessValidation := c.CheckAssignedDataAccess(contract.Data)
+
+	serviceValidationScore := c.GetServiceValidationScore(nameValidation, repositoryValidation, accessValidation)
+
+	if serviceValidationScore < MinimumThresholdScore {
+		overallContractValidity = Invalid
+	}
+
+	checks := validity.Checks{
+		Valid:       &overallContractValidity,
+		DataAccess:  &accessValidation,
+		ServiceName: &nameValidation,
+		Repository:  &repositoryValidation,
+	}
+
+	return checks
+}
