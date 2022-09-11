af699685b317cc8e04755dbe78bbe8b1dcde334e
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-09-11 23:15:57 +0530

    Added Crypto(RSA) Helper

--- onboarding/integration/src/helpers/crypto.js
+++ onboarding/integration/src/helpers/crypto.js
@@ -7,6 +7,14 @@ export default class RSA {
         return crypto.generateKeyPairSync("rsa", {
             // The standard secure default length for RSA keys is 2048 bits
             modulusLength: 2048,
+            publicKeyEncoding: {
+                type: 'spki',
+                format: 'pem'
+            },
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: 'pem'
+            }
         })
     }
 }
